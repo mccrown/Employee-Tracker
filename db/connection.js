fa939f9ada4a2ee297c72b1a30b491c678448f99
CMcCown <connermccown94@gmail.com>
2022-04-17 16:24:59 -0600

    added tables to schema file

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      port: 3306,
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
